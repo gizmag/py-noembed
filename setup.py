089c3d3c02330264845e0e3228fbdec854abfe6b
Jacob Haslehurst <jacob@haslehurst.net>
2013-07-29 17:17:50 +1000

    adding in setup file + travis config

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='py-noembed',
+    version='0.1',
+    description='Python Wrapper over NoEmbed',
+    author='Jacob Haslehurst',
+    author_email='jacob@haslehurst.net',
+    url='https://github.com/gizmag/py-noembed',
+    packages=['noembed'],
+    install_requires=['requests']
+)
