3513d658cdedf26d165c0c97de2461d50fafbf90
Jacob Haslehurst <jacob@haslehurst.net>
2013-07-29 17:47:09 +1000

    adding tests for embed method

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,4 @@
+from mock import patch, Mock
 from unittest import TestCase
 import noembed
 
@@ -25,3 +26,47 @@ class NoEmbedTests(TestCase):
     def test_payload_includes_max_height_when_included(self):
         payload = self.noembed.build_payload(self.example_url, max_height=100)
         self.assertEqual(payload['maxheight'], 100)
+
+    @patch('noembed.requests')
+    def test_requests_get_is_called_with_endpoint_url_and_payload(self, mock_requests):
+        mock_response = Mock()
+        mock_response.json.return_value = {}
+        mock_requests.get.return_value = mock_response
+        self.noembed.embed(self.example_url, 100, 100)
+        mock_requests.get.assert_called_once_with(
+            noembed.NOEMBED_ENDPOINT_URL,
+            params={'url': self.example_url, 'maxwidth':100, 'maxheight':100}
+        )
+
+    @patch('noembed.requests')
+    def test_excption_raised_if_error_key_in_json(self, mock_requests):
+        mock_response = Mock()
+        mock_response.json.return_value = {
+            'url': 'http://example.com',
+            'error': 'This is an example error'
+        }
+        mock_requests.get.return_value = mock_response
+
+        with self.assertRaises(noembed.NoEmbedException):
+            self.noembed.embed(self.example_url)
+
+    @patch('noembed.requests')
+    def test_embed_returns_a_noembed_response(self, mock_requests):
+        mock_response = Mock()
+        mock_response.json.return_value = {}
+        mock_requests.get.return_value = mock_response
+
+        resp = self.noembed.embed(self.example_url)
+
+        self.assertTrue(isinstance(resp, noembed.NoEmbedResponse))
+
+    @patch('noembed.NoEmbedResponse')
+    @patch('noembed.requests')
+    def test_embed_passes_response_dict_to_noembed_response(self, mock_requests, mock_noembed_response):
+        mock_response = Mock()
+        mock_response.json.return_value = {'foo': 'bar'}
+        mock_requests.get.return_value = mock_response
+
+        noembed.embed(self.example_url)
+
+        mock_noembed_response.assert_called_once_with({'foo': 'bar'})
